29d9dd860b7e0fb96056f2024f777a58080c0ea5
Jacob Colyvan <jacob_colyvan@live.com>
2020-09-06 01:34:05 +1000

    added error messages to input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 
-const Input = ({ title, saveParam, limit, setError }) => {
+const Input = ({ title, saveParam, limit }) => {
   let label = `${title}`
   if (limit !== 1) label = `${title} (0 - ${limit})`;
+
+  const [error, setError] = useState(false)
+
+  const validateInput = (e) => {
+    console.log(e.target.value)
+    if (!e.target.value || (e.target.value >= 0 && e.target.value < limit) ) {
+      setError(false)
+      saveParam(title, e.target.value, limit)
+    } else {
+      setError(true)
+    } 
+  }
 
   return (
     <>
@@ -11,11 +23,10 @@ const Input = ({ title, saveParam, limit, setError }) => {
         fullWidth
         label={label}
         type='number'
-        onChange={(e) => {
-          
-          saveParam(title, e.target.value, limit);
-        }}
+        onChange={validateInput}
         style={{ flex: 1, margin: '4px 20px 0 0', color: 'white'}}
+        error={error}
+        helperText={error ? "Invalid range" : false}
       />
     </>
   );
